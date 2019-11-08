31bbdea8770f092b093f476c56a5bedabac54c7a
DanyaYashin <yashindan2000@gmail.com>
2019-11-08 14:51:51 +0400

    New base

--- arithmetic/admin.py
+++ arithmetic/admin.py
@@ -1,24 +1,30 @@
 from django.contrib import admin
-from .models import Rule, Student
+from .models import Block, Subject, Rule, Student
 
 # Register your models here.
+class BlockInLine(admin.TabularInline):
+    model=Block
+    extra=1
+
 class RuleAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Name', {'fields':['rule_name']}),
-        ('Text', {'fields':['rule_text']}),
-        ('Image',{'fields':['rule_image']}),
     ]
-'''
-class RulesInline(admin.StackedInline):
-    model = Rule
-    extra = 1
-'''
+    inline = [BlockInLine]
+
 class StudentAdmin(admin.ModelAdmin):
+    filter_horizontal=('rules_set',)
     fieldsets=[
     ('Name',{'fields':['student_name']}),
     ('Vocabulary',{'fields':['rules_set']})
     ]
 
+class SubjectAdmin(admin.ModelAdmin):
+    fieldsets=[
+    ('Name', {'fields':['subject_name']}),
+    ]
+
 
 admin.site.register(Rule, RuleAdmin)
 admin.site.register(Student, StudentAdmin)
+admin.site.register(Subject, SubjectAdmin)
--- arithmetic/models.py
+++ arithmetic/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Subject(models.Model):
+    def __str__(self):
+        return self.subject_name
+    subject_name = models.CharField(max_length=50)
+
+
 class Rule(models.Model):
     def __str__(self):
-        return self.rule_name
-    rule_name=models.CharField(max_length=50)
-    rule_text=models.TextField()
-    rule_image=models.ImageField(blank=True)
+        return self.subject_name
+    rule_name = models.CharField(max_length=50)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+class Block(models.Model):
+    def __str__(self):
+        return self.block_text
+    rule=models.ForeignKey(Rule, on_delete=models.CASCADE)
+    block_text=models.TextField()
+    block_image=models.ImageField(blank=True)
 
 class Student(models.Model):
     def __str__(self):
